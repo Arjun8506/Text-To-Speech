bdcc95cd2a54d147c84ae7749261ca04d2de4d7e
Arjun8506 <140004161+Arjun8506@users.noreply.github.com>
2024-02-07 17:10:51 +0530

    fully responsive too

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const btn = document.querySelector("#btn");
     const text = document.querySelector("#text");
+    const languages = document.querySelector("#languages");
   
     btn.addEventListener("click", () => {
       const speech = new SpeechSynthesisUtterance();
-      speech.lang = 'en-US'; // Set language (optional)
+      speech.lang = languages.value; // Set language (optional)
       speech.text = text.value;
       window.speechSynthesis.speak(speech);
       console.log(text.value);
