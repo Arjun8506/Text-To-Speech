65ab485ad21312669c279a07df1c329a3dc6cc64
Arjun8506 <140004161+Arjun8506@users.noreply.github.com>
2024-02-07 16:40:21 +0530

    created beautiful ui

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-let btn = document.querySelector("#btn")
-let text = document.querySelector("#text")
-let copy = document.querySelector("#copy")
-let languages = document.querySelector("#languages")
-
-console.log(languages);
-
-
-btn.addEventListener("click", ()=>{
-    let speech = new SpeechSynthesisUtterance();
-    speech.lang = "en-US";
-    speech.text = text.value;
-    window.speechSynthesis.speak(speech);
-})
-
-// copy.addEventListener("click", ()=>{
-//    let selected = text.select();
-//   document.execCommand('copy');
-// })
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.querySelector("#btn");
+    const text = document.querySelector("#text");
+  
+    btn.addEventListener("click", () => {
+      const speech = new SpeechSynthesisUtterance();
+      speech.lang = 'en-US'; // Set language (optional)
+      speech.text = text.value;
+      window.speechSynthesis.speak(speech);
+      console.log(text.value);
+    });
+  });
+  
